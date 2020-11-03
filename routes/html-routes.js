730d093563cae960ab44433f474d7f5ac2bf5478
Jack Murphy <signal.space@protonmail.com>
2020-11-03 14:26:13 -0600

    changes lets to vars just to check if example works

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,8 +1,8 @@
 // Requiring path to so we can use relative routes to our HTML files
-let path = require("path");
+var path = require("path");
 
 // Requiring our custom middleware for checking if a user is logged in
-let isAuthenticated = require("../config/middleware/isAuthenticated");
+var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
 
