9438179c5deca73c1588ca32e3e584a632d9413d
Raunel Suarez-Magana <rsuarez_26@utexas.edu>
2020-11-06 00:41:11 -0600

    small fix to users retrieving saved videos

--- public/js/scene.js
+++ public/js/scene.js
@@ -166,7 +166,7 @@ function retrieveVideos(){
         $("#container").html("<div id=\"blocker\"></div>")
         init(videoIdArr);
         animate();
-        renderButtons;
+        renderButtons();
     })
 }
 
